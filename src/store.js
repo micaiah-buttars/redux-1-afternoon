1421e4f6531f105e32acb876f33fcebc5bc985f2
Micaiah Buttars <michael.buttars@gmail.com>
2019-04-01 14:04:41 -0600

    completed step 5

--- src/store.js
+++ src/store.js
@@ -1,1 +1,31 @@
+import {createStore} from 'redux'
 
+const initialState = {
+    name: '',
+    category: '',
+    authorFirst: '',
+    authorLast: ''
+}
+
+export const UPDATE_NAME = 'UPDATE_NAME'
+export const UPDATE_CATEGORY = 'UPDATE_CATEGORY'
+export const UPDATE_AUTHOR_FIRST = 'UPDATE_AUTHOR_FIRST'
+export const UPDATE_AUTHOR_LAST = 'UPDATE_AUTHOR_LAST'
+
+
+function reducer(state = initialState, action){
+    switch(action.type){
+        case UPDATE_NAME:
+            return {...state, name: action.payload}
+        case UPDATE_CATEGORY:
+            return {...state, category: action.payload}
+        case UPDATE_AUTHOR_FIRST:
+            return {...state, category: action.payload}
+        case UPDATE_AUTHOR_LAST:
+            return {...state, category: action.payload}
+    }
+}
+
+
+
+export default createStore(reducer)
